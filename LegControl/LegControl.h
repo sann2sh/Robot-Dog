b68a527e2e10f2eba3003b404595c23ed9f45172
Santosh <raysanto12345@gmail.com>
2025-06-12 23:26:08 +0545

    fix: updated joint angle limits

--- LegControl/LegControl.h
+++ LegControl/LegControl.h
@@ -11,6 +11,10 @@
 #define HIP_LEG_JOINT    1
 #define HIP_BODY_JOINT   0
 
+
+#define NUM_LEGS 4
+#define NUM_JOINTS 3
+
 #define LEG_1 0
 #define LEG_2 1
 #define LEG_3 2
@@ -22,12 +26,14 @@
 
 
 // Joint Angle Limits
-#define KNEE_JOINT_ANGLE_MIN        0// 75 mid point
-#define KNEE_JOINT_ANGLE_MAX        150
-#define HIP_LEG_JOINT_ANGLE_MIN     -247// -145 mid point
-#define HIP_LEG_JOINT_ANGLE_MAX     -43
-#define HIP_BODY_JOINT_ANGLE_MIN    -43
-#define HIP_BODY_JOINT_ANGLE_MAX    37
+#define KNEE_JOINT_ANGLE_MIN        30
+#define KNEE_JOINT_ANGLE_MAX        180
+
+#define HIP_LEG_JOINT_ANGLE_MIN     -150
+#define HIP_LEG_JOINT_ANGLE_MAX     30    
+
+#define HIP_BODY_JOINT_ANGLE_MIN    -20
+#define HIP_BODY_JOINT_ANGLE_MAX    20
 
 void initializeLeg();
 void moveLeg(int leg, int joint, int angle);
