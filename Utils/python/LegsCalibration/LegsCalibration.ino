03e40b2761fb560d0da4a471e6140e2695b19930
JosephThapa0617 <65438997+JosephThapa0617@users.noreply.github.com>
2025-06-18 20:04:06 +0545

    feat: CoppeliaSim, Fusion360 Design & Arduino Interpolation

--- Utils/python/LegsCalibration/LegsCalibration.ino
+++ Utils/python/LegsCalibration/LegsCalibration.ino
@@ -1,14 +1,28 @@
-#include <LegControl.h>
+#include <LegControl.h>  // your existing servo handling library
 
 #define NUM_LEGS 4
 #define NUM_JOINTS 3
+#define TOTAL_JOINTS (NUM_LEGS * NUM_JOINTS)
+#define INTERPOLATION_STEPS 50
+#define STEP_DELAY 100  // ms between steps
+
+int currentAngles[NUM_LEGS][NUM_JOINTS] = {
+  {105, -60, 0},  // Leg 0
+  {105, -60, 0},
+  {105, -60, 0},
+  {105, -60, 0}
+};
 
 void setup() {
-  Serial.begin(2000000);
-  while (!Serial); // Wait for serial on Leonardo-type boards
+  Serial.begin(9600);
+  while (!Serial);
   Serial.println("Ready to receive joint angles...");
-  initializeLeg();  // Setup servos
+  initializeLeg();  // Your function to attach and configure all servos
+}
 
+// Easing function for smooth motion (cosine ease-in-out)
+float easeInOut(float t) {
+  return 0.5 * (1 - cos(t * PI));
 }
 
 void loop() {
@@ -23,35 +37,50 @@ void loop() {
       inputString = "";
     }
 
-
     else if (ch == '>' && receiving) {
       receiving = false;
 
-      int angles[NUM_LEGS][NUM_JOINTS];
-      int values[NUM_LEGS * NUM_JOINTS];
+      int values[TOTAL_JOINTS];
       int index = 0;
-
       char* token = strtok((char*)inputString.c_str(), ",");
 
-
-      while (token != NULL && index < NUM_LEGS * NUM_JOINTS) {
+      while (token != NULL && index < TOTAL_JOINTS) {
         values[index++] = atoi(token);
         token = strtok(NULL, ",");
       }
 
-      if (index == NUM_LEGS * NUM_JOINTS) {
+      if (index == TOTAL_JOINTS) {
+        int targetAngles[NUM_LEGS][NUM_JOINTS];
         int k = 0;
         for (int i = 0; i < NUM_LEGS; i++) {
-          angles[i][KNEE_JOINT] = values[k++];
-          angles[i][HIP_LEG_JOINT] = values[k++];
-          angles[i][HIP_BODY_JOINT] = values[k++];
+          targetAngles[i][KNEE_JOINT]     = values[k++];
+          targetAngles[i][HIP_LEG_JOINT]  = values[k++];
+          targetAngles[i][HIP_BODY_JOINT] = values[k++];
         }
 
-        for (int leg = 0; leg < NUM_LEGS; leg++) {
-          moveLeg(leg, KNEE_JOINT, angles[leg][KNEE_JOINT]);
-          moveLeg(leg, HIP_LEG_JOINT, angles[leg][HIP_LEG_JOINT]);
-          moveLeg(leg, HIP_BODY_JOINT, angles[leg][HIP_BODY_JOINT]);
+        // Interpolate smoothly from current to target
+        for (int step = 0; step <= INTERPOLATION_STEPS; step++) {
+          float t = (float)step / INTERPOLATION_STEPS;
+          float eased = easeInOut(t);
+
+          for (int leg = 0; leg < NUM_LEGS; leg++) {
+            for (int joint = 0; joint < NUM_JOINTS; joint++) {
+              int start = currentAngles[leg][joint];
+              int end   = targetAngles[leg][joint];
+              int interpolated = start + (end - start) * eased;
+              moveLeg(leg, joint, interpolated);
+            }
+          }
+          delay(STEP_DELAY);
         }
+
+        // Update current angles
+        for (int leg = 0; leg < NUM_LEGS; leg++) {
+          for (int joint = 0; joint < NUM_JOINTS; joint++) {
+            currentAngles[leg][joint] = targetAngles[leg][joint];
+          }
+        }
+
       } else {
         Serial.println("Invalid angle data.");
       }
